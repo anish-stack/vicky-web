d2cc11a03c2ebeaafeedc567b0b73f7ed76fc397
Coderhitesh <hiteshy468@gmail.com>
2026-03-07 15:17:32 +0530

    add

--- web/src/themes/driver-modern/components/Header.tsx
+++ web/src/themes/driver-modern/components/Header.tsx
@@ -28,7 +28,7 @@ const Header = () => {
       {/* Top bar */}
       <div className="bg-yellow-500 dark:bg-yellow-600 text-black dark:text-gray-900 py-2 px-4">
         <div className="max-w-7xl mx-auto flex flex-wrap justify-between items-center text-sm">
-          <div className="flex items-center space-x-4">
+          <div className="flex items-center justify-between space-x-4 w-full sm:w-auto">
             <div className="flex items-center space-x-1">
               <Phone className="h-3 w-3" />
               <span>{whatsapp}</span>
@@ -38,7 +38,7 @@ const Header = () => {
               <span>{email}</span>
             </div>
           </div>
-          <div className="flex items-center space-x-1">
+          <div className="hidden sm:flex items-center space-x-1">
             <MapPin className="h-3 w-3" />
             <span>24/7 Service Available</span>
           </div>
